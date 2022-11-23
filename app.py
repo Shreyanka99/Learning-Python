d80602f956495e3008864ec68bcd459a2c1936f0
Shreyanka <shreyanka99@gmail.com>
2022-11-24 00:18:57 +0530

    Modified app.py by adding comments

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+# CAR CONTROL GAME USING COMMANDS
 command = ""
+# stating required initial conditions
 start = False
 right = False
 left = False
@@ -15,7 +17,9 @@ Enter instructions to move your car:
         forward - going straight
         reverse - taking reverse
         u turn - taking u turn
+        quit- leave the program
 ''')
+# checking necessary conditions to continue loop for entering different instructions
 while command != 'quit' or command:
     command = input(">>")
     if command == 'help':
@@ -28,44 +32,53 @@ while command != 'quit' or command:
         forward - going straight
         reverse - taking reverse
         u turn - taking u turn''')
+# car has started
     elif command == 'start':
         if start:
             print("already started")
         else:
             start = True
             print("started")
+# car is asked to stop
     elif command == 'stop' and start:
         print("stopped")
         stop = True
         start = False
+# car is asked to move right/left
     elif command == 'right' and start:
         if not right or forward:
             print("taking right")
             right = True
+# if car is already taking right, it cant take another right, either a u turn, or moving forward then a right
         elif not forward and right:
             print("already taking right,first move forward and then take right or take U turn ")
     elif command == 'left' and start:
         if not left or forward:
             print("taking left")
             left = True
+# car is asked to move forward or reverse
         elif not forward and left:
             print("already taking left,first move forward and then take left")
     elif command == 'forward' and start:
         print("going forward")
         forward = True
+# parking the car
     elif command == 'park' and start:
         if not start or park:
             print('already parked')
         else:
             print('parking')
             park = True
+# taking U turn
     elif command == 'u turn' and start:
         if (right or left) and not forward:
             print("completing the u turn")
         else:
             print('taking U turn')
+# taking reverse
     elif command == 'reverse' and start:
         print('taking reverse')
+# leaving the game
     elif command == 'quit':
         exit()
     else:
